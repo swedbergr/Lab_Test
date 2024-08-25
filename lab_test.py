4df434fd3d1f8f94af7dc8a0ea27d76df9a51d5b
Riley Swedberg <swedbergr@unk.edu>
2024-08-25 19:25:38 +0000

    Lab Ready

--- lab_test.py
+++ lab_test.py
@@ -1,1 +1,11 @@
+# Create a test program to verify GitHub Classroom works!
 
+
+# Get a name to use in the program
+name = input("What is your name? ")
+
+# Type some message to the name inside the quotation marks
+message = ""
+
+# Display message to name
+print(f"Hello, {name}! {message}")
